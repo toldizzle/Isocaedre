ed288e959aad362bfdfdb97b3ff6943cdd793e26
Francis <toldizzle@hotmail.com>
2015-10-02 14:10:17 -0400

    membre/ post

---
+++ ClubRP/Models/Membre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubRP.Models
+{
+    public class Membre
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime Depuis { get; set; }
+        public DateTime Age { get; set; }
+        public bool Maitre { get; set; }
+
+    }
+}
---
+++ ClubRP/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubRP.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Titre { get; set; }
+        public string Message { get; set; }
+        public DateTime Creation { get; set; }
+        public int NbReponse { get; set; }
+
+    }
+}
