d5a94dff1eda95dc61b24083ecd7872069e1d434
David Bisaillon <davidbisaillon@hotmail.com>
2015-12-01 10:27:08 -0500

    add migration

---
+++ ClubRP/Migrations/ApplicationDBContext/201512011354399_CreatePersonnages.cs
@@ -0,0 +1,16 @@
+namespace ClubRP.Migrations.ApplicationDBContext
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePersonnages : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
