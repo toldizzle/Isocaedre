bf3e4e97a679a66afd5fc2629f5f6cf50b6bd370
David Bisaillon <davidbisaillon@hotmail.com>
2015-12-03 02:24:57 -0500

    Fiche perso Details.cshtml

--- ClubRP/Migrations/ApplicationDBContext/201512011354399_CreatePersonnages.Designer.cs
+++ ClubRP/Migrations/ApplicationDBContext/201512011354399_CreatePersonnages.Designer.cs
@@ -18,7 +18,7 @@ namespace ClubRP.Migrations.ApplicationDBContext
         
         string IMigrationMetadata.Source
         {
-            get { return Resources.GetString("Source"); }
+            get { return null; }
         }
         
         string IMigrationMetadata.Target
